0a3c50b0d27954d6fe265fd4934e364d30473d9c
Avery Pierce <apierce@partstown.com>
2018-04-17 12:30:57 -0500

    Add an "express" function to the WebServer object that returns a valid express object

--- openapi-webserver/index.js
+++ openapi-webserver/index.js
@@ -8,18 +8,23 @@ function OpenAPIWebserver(filePath) {
     this.filePath = filePath;
 }
 
+OpenAPIWebserver.prototype.express = function() {
+	const app = express();
+	app.get('/openapi.json', (request, response) => {
+		Specification.readFile(this.filePath, (error, spec) => {
+			response.send(spec.resolvedJSON);
+			response.end();
+		});
+	});
+
+	const staticPath = path.join(__dirname, 'static');
+	app.use(express.static(staticPath));
+
+	return app;
+};
+
 OpenAPIWebserver.prototype.listen = function(port, callback) {
-
-    const app = express();
-    app.get('/openapi.json', (request, response) => {
-        Specification.readFile(this.filePath, (error, spec) => {
-            response.send(spec.resolvedJSON);
-            response.end();
-        });
-    });
-
-    const staticPath = path.join(__dirname, 'static');
-    app.use(express.static(staticPath));
+    const app = this.express();
     app.listen(port, callback);
 };
 
